e7c57956f0c75a7c7e3782974e0c5c62c805191e
SammyK4488 <skleant1@students.towson.edu>
2025-06-13 16:20:49 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,8 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProfilePage from './app/ProfilePage.tsx';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {}
-        <Stack.Screen name="Profile" component={ProfilePage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return <ProfilePage />;
 }
