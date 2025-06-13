f67aa080d51d078197384fe6297ed68528509021
SammyK4488 <skleant1@students.towson.edu>
2025-06-13 16:18:07 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProfilePage from './app/ProfilePage.tsx';
-// import other screens as needed
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +8,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* Add existing screens here */}
+        {}
         <Stack.Screen name="Profile" component={ProfilePage} />
       </Stack.Navigator>
     </NavigationContainer>
