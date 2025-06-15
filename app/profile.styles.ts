3f5f3a7f14a1011c499c525c91658b3af5090508
SammyK4488 <skleant1@students.towson.edu>
2025-06-15 15:06:46 -0400

    Update profile.styles.ts
    Profile Image Specs Added

--- app/profile.styles.ts
+++ app/profile.styles.ts
@@ -9,6 +9,12 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 18,
   },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 20,
+  },
 });
 
 export default styles;
