4595647eae93ad174e15286f08cd3d2a8ac1b89a
SammyK4488 <skleant1@students.towson.edu>
2025-06-15 14:14:37 -0400

    Delete App.tsx

--- App.tsx
+++
@@ -1,8 +0,0 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ProfilePage from './app/ProfilePage.tsx';
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-    return <ProfilePage />;
-}
