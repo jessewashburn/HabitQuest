6fc229986005f29a9193f8e5ec6ba26a7f670a3a
SammyK4488 <skleant1@students.towson.edu>
2025-06-15 15:06:09 -0400

    Update profile.tsx
    User Icon Added

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import { Alert, Button, Text, View } from 'react-native';
+import { Alert, Button, Image, Text, View } from 'react-native';
 import styles from './profile.styles.ts';
 
 interface User {
   name: string;
   points: number;
   level: number;
+  profileImage?: string;
 }
 
 interface ProfileScreenProps {
@@ -20,6 +21,7 @@ export default function ProfileScreen({ user, readOnly = false }: ProfileScreenP
     name: "User Profile",
     points: 0,
     level: 1,
+    profileImage: "https://private-user-images.githubusercontent.com/142361664/453683987-afde7c2b-8220-492a-8ab0-cb6c8e247725.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTAwMTQxNTQsIm5iZiI6MTc1MDAxMzg1NCwicGF0aCI6Ii8xNDIzNjE2NjQvNDUzNjgzOTg3LWFmZGU3YzJiLTgyMjAtNDkyYS04YWIwLWNiNmM4ZTI0NzcyNS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNjE1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDYxNVQxODU3MzRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT03NzM3OThiYzdlOTY2ZTIwODFiODk3YzY2ZmQwYmIyMTljYmQ1NzRhZGFkNWViNGE4ZjQxMDEwMDEwYjhkNGJkJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.8poW-ufKISgEsdm4oO98JdyzMOui6ddmeeU5GYlpnLs",
   };
 
   const handleSave = () => {
@@ -37,6 +39,10 @@ export default function ProfileScreen({ user, readOnly = false }: ProfileScreenP
       <Text style={styles.text}>{userData.name}</Text>
       <Text style={styles.text}>Points: {userData.points}</Text>
       <Text style={styles.text}>Level: {userData.level}</Text>
+      <Image
+  source={{ uri: userData.profileImage }}
+  style={styles.profileImage}
+/>
 
       <Text style={styles.text}>Theme: {theme}</Text>
       <Button
