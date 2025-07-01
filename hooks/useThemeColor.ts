b57fd403003eaa0cb094bf43e362338bea71d6b2
SammyK4488 <skleant1@students.towson.edu>
2025-06-30 21:18:35 -0400

    Update useThemeColor.ts

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -3,15 +3,15 @@
  * https://docs.expo.dev/guides/color-schemes/
  */
 
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from '@/constants/Colors.ts';
+import { useTheme } from './ThemeContext.tsx';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-  const theme = useColorScheme() ?? 'light';
-  const colorFromProps = props[theme];
+  const { theme } = useTheme();
+  const colorFromProps = props[theme as 'light' | 'dark'];
 
   if (colorFromProps) {
     return colorFromProps;
